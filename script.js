ab5a25b402dd9b0565aa18bf287a41d87c5c873f
estebanLoza <estebanm.hloza@gmail.com>
2026-08-09 02:43:39 -0600

    Refactor mobile menu functionality to ensure it closes when a navigation link is clicked

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //left menu mobile
-
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -10,6 +10,11 @@ menuOpenButton.addEventListener("click", () => {
 
 // Close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Close menu when the nav link is clicked
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
 
 //Código swiper (slide galery)
 const swiper = new Swiper(".slider-wrapper", {
